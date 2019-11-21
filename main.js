ce0b592967cddfeee28b2358e2a4db9db49aa61f
Brais Cidras <braisomary@gmail.com>
2019-11-21 15:53:33 +0200

    Section 05. Event Handling

--- main.js
+++ main.js
@@ -9,8 +9,34 @@ var app = new Vue({
 		link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
 		inventory: 100,
 		onSale: true,
-		details: ["80% cotton", "20% polyester", "Gender-neutral"],
-		variants: [{variantId: 2234, variantColor: "green"}, {variantId: 2235, variantColor: "blue"}],
-		sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
+		details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+		variants: [
+			{
+				variantId: 2234, 
+				variantColor: 'green',
+				variantImage: './assets/vmSocks-green.jpg'
+			}, 
+			{	
+				variantId: 2235, 
+				variantColor: 'blue',
+				variantImage: './assets/vmSocks-blue.jpg'
+			}
+		],
+		sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+		cart: 0
+	},
+	methods: {
+		addToCart: function () {
+			this.cart += 1;
+		},
+		changeImage: function(image) {
+			this.image = image;
+		},
+		removeFromCart: function () {
+			if (!this.cart)
+				return;
+			this.cart -= 1;
+		},
+
 	}
 });

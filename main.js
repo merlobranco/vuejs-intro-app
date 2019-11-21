9f2bc2af15b0b26ab77d172206e3a59d23f2affd
Brais Cidras <braisomary@gmail.com>
2019-11-21 15:11:45 +0200

    Section 03. Conditional Rendering

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ var app = new Vue({
 		product: 'Socks',
 		description: 'A pair of warm, fuzzy socks',
 		image: './assets/vmSocks-green.jpg',
-		link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks'
+		link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
+		inventory: 100,
+		onSale: true
 	}
 });

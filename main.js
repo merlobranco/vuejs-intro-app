443b36cfa8429fea1634a6457b02d80899d685f3
Brais Cidras <braisomary@gmail.com>
2019-11-25 08:45:21 +0200

    Section 10. Forms. Better Approach

--- main.js
+++ main.js
@@ -2,13 +2,54 @@
 
 Vue.component('product-review', {
   	template: `
-  		<input v-model="name">
+  		<form class="review-form" @submit.prevent="onSubmit">
+	      <p>
+	        <label for="name">Name:</label>
+	        <input id="name" v-model="name" placeholder="name">
+	      </p>
+	      
+	      <p>
+	        <label for="review">Review:</label>      
+	        <textarea id="review" v-model="review"></textarea>
+	      </p>
+	      
+	      <p>
+	        <label for="rating">Rating:</label>
+	        <select id="rating" v-model.number="rating">
+	          <option>5</option>
+	          <option>4</option>
+	          <option>3</option>
+	          <option>2</option>
+	          <option>1</option>
+	        </select>
+	      </p>
+	          
+	      <p>
+	        <input type="submit" value="Submit">  
+	      </p>    
+	    
+	    </form>
   	`,
   	data() { 
 		return {
-			name: null
+			name: null,
+			review: null,
+			rating: null
 		}
 	},
+	methods: {
+		onSubmit() {
+      		let productReview = {
+        		name: this.name,
+        		review: this.review,
+        		rating: this.rating
+      		}
+      		this.$emit('review-submitted', productReview);
+      		this.name = null;
+      		this.review = null;
+      		this.rating = null;
+    	}
+	}
 })
 
 
@@ -71,7 +112,20 @@ Vue.component('product', {
 						@click="removeFromCart"
 						:class="{ disabledButton: !added }">Remove from cart</button>
 			</div>
-			<product-review></product-review>
+
+			<div>
+        		<h2>Reviews</h2>
+		        <p v-if="!reviews.length">There are no reviews yet.</p>
+		        <ul>
+		          	<li v-for="review in reviews">
+		          		<p>{{ review.name }}</p>
+		          		<p>Rating: {{ review.rating }}</p>
+		          		<p>{{ review.review }}</p>
+		          	</li>
+		        </ul>
+			</div>
+
+			<product-review @review-submitted="addReview"></product-review>
 		</div>
 	`,
 	data() { 
@@ -100,7 +154,8 @@ Vue.component('product', {
 			],
 			sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
 			onSale: true,
-			added: false
+			added: false,
+			reviews: []
 		}
 	},
 	methods: {
@@ -114,6 +169,9 @@ Vue.component('product', {
 		removeFromCart: function () {
 			this.added = false;
 			this.$emit('remove-from-cart', this.variants[this.selectedVariant].variantId);
+		},
+		addReview: function (review) {
+			this.reviews.push(review);
 		}
 	},
 	computed: {

3dcd9ab5795063e5bc33ce4cc1f971a02845e2d2
Brais Cidras <braisomary@gmail.com>
2019-11-25 09:39:27 +0200

    Section 10. Forms. Custom Validation

--- main.js
+++ main.js
@@ -3,30 +3,49 @@
 Vue.component('product-review', {
   	template: `
   		<form class="review-form" @submit.prevent="onSubmit">
-	      <p>
-	        <label for="name">Name:</label>
-	        <input id="name" v-model="name" placeholder="name">
-	      </p>
-	      
-	      <p>
-	        <label for="review">Review:</label>      
-	        <textarea id="review" v-model="review"></textarea>
-	      </p>
-	      
-	      <p>
-	        <label for="rating">Rating:</label>
-	        <select id="rating" v-model.number="rating">
-	          <option>5</option>
-	          <option>4</option>
-	          <option>3</option>
-	          <option>2</option>
-	          <option>1</option>
-	        </select>
-	      </p>
-	          
-	      <p>
-	        <input type="submit" value="Submit">  
-	      </p>    
+
+  			<p v-if="errors.length" style="color: #e74c3c;">
+      			<b>Please correct the following error(s):</b>
+      			<ul>
+        			<li v-for="error in errors">{{ error }}</li>
+      			</ul>
+    		</p>
+
+
+		    <p>
+		    	<label for="name">Name:</label>
+		        <input id="name" v-model="name" placeholder="name">
+		    </p>
+		      
+		    <p>
+		        <label for="review">Review:</label>      
+		        <textarea id="review" v-model="review"></textarea>
+		    </p>
+		      
+		    <p>
+		        <label for="rating">Rating:</label>
+		        <select id="rating" v-model.number="rating">
+		          	<option>5</option>
+		          	<option>4</option>
+		          	<option>3</option>
+		          	<option>2</option>
+		          	<option>1</option>
+		        </select>
+		    </p>
+
+		    <p>Would you recommend this product?</p>
+        		<label>
+          			Yes
+          			<input type="radio" value="Yes" v-model="recommend"/>
+        		</label>
+        		<label>
+          			No
+          			<input type="radio" value="No" v-model="recommend"/>
+        		</label>
+		          
+		    <p>
+		        <input type="submit" value="Submit">  
+		    </p>    
 	    
 	    </form>
   	`,
@@ -34,20 +53,36 @@ Vue.component('product-review', {
 		return {
 			name: null,
 			review: null,
-			rating: null
+			rating: null,
+			recommend: null,
+			errors: []
 		}
 	},
 	methods: {
 		onSubmit() {
-      		let productReview = {
-        		name: this.name,
-        		review: this.review,
-        		rating: this.rating
-      		}
-      		this.$emit('review-submitted', productReview);
-      		this.name = null;
-      		this.review = null;
-      		this.rating = null;
+			if (this.name && this.review && this.rating && this.recommend) {
+				let productReview = {
+	        		name: this.name,
+	        		review: this.review,
+	        		rating: this.rating,
+	        		recommend: this.recommend
+	      		}
+	      		this.$emit('review-submitted', productReview);
+	      		this.name = null;
+	      		this.review = null;
+	      		this.rating = null;
+	      		this.recommend = null;
+			}
+			else {
+				if (!this.name)
+					this.errors.push("Name required.");
+				if (!this.review)
+					this.errors.push("Review required.");
+				if (!this.rating)
+					this.errors.push("Rating required.");
+				if (!this.recommend)
+					this.errors.push("Recommendation required.");
+			}
     	}
 	}
 })
@@ -121,6 +156,8 @@ Vue.component('product', {
 		          		<p>{{ review.name }}</p>
 		          		<p>Rating: {{ review.rating }}</p>
 		          		<p>{{ review.review }}</p>
+		          		<p v-if="review.recommend == 'Yes'">Recommended</p>
+		          		<p v-else>No Recommended</p>
 		          	</li>
 		        </ul>
 			</div>
